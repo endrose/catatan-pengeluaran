5184d078fe8361c7f00848d3c4cf3849e39496d6
Endrose <endros007@gmail.com>
2021-01-19 02:22:24 +0700

    convert option api to compostion api

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <Title title="Catatan Pengeluaran" />
   <FormPengeluaran @entry-pengeluaran="entryPengeluaran($event)" />
-  <h2>Total Pengeluaran : {{ totalPengeluaran }}</h2>
+  <h2 class="t-pengeluaran">Total Pengeluaran : {{ totalPengeluaran }}</h2>
   <h2 v-if="warning" class="warning">Pengeluaran Anda Terlalu Banyak</h2>
   <ListPengeluaran v-if="showList" :dataPengeluaran="dataPengeluaran" />
   <div v-else>Bagus kamu menghemat uang! untuk pengeluaran lain.</div>
@@ -11,7 +11,7 @@
 import Title from "./components/Title.vue";
 import ListPengeluaran from "./components/ListPengeluaran.vue";
 import FormPengeluaran from "./components/FormPengeluaran.vue";
-
+import {ref, computed, watch} from "vue";
 export default {
   name: "App",
   components: {
@@ -19,48 +19,82 @@ export default {
     ListPengeluaran,
     FormPengeluaran,
   },
-  data() {
-    return {
-      warning:true,
-      dataPengeluaran: [
-        {
-          nominal: 20000,
-          keterangan: "Makan Siang",
-        },
-        {
-          nominal: 25000,
-          keterangan: "Beli Pulsa",
-        },
-        {
-          nominal: 15000,
-          keterangan: "Beli Bensin",
-        },
-      ],
-    };
-  },
+  // COMPOSITON API
+  setup(){
+    const warning = ref(false);
+    const dataPengeluaran = ref([]);
+    const showList = computed(()=> dataPengeluaran.value.length > 0)
+    const totalPengeluaran = computed(() => dataPengeluaran.value.reduce((acc,item) => acc+parseInt(item.nominal),0))
 
-  methods: {
-   entryPengeluaran(event){
 
-     this.dataPengeluaran.push(event);
-   }
-  },
-  computed: {
-    showList: function() {
-      return this.dataPengeluaran.length > 0;
-    },
-    totalPengeluaran:function(){
-      return this.dataPengeluaran.reduce((acc, item) => acc + parseInt(item.nominal),0);
+    function entryPengeluaran(event){
+      dataPengeluaran.value.push(event)
     }
-  },
-  watch:{
-    totalPengeluaran:function(val){
-      if(val > 100000){
-          this.warning= true
+
+
+    watch(totalPengeluaran, (newValue)=> {
+      if(newValue > 100000){
+        warning.value = true
       }
+    });
+
+
+    return {
+      dataPengeluaran,
+      showList,
+      totalPengeluaran,
+      entryPengeluaran,
+      warning
     }
   }
+
+  // OPTION API
+  // data() {
+  //   return {
+  //     warning:true,
+  //     dataPengeluaran: [
+  //       // {
+  //       //   nominal: 20000,
+  //       //   keterangan: "Makan Siang",
+  //       // },
+  //       // {
+  //       //   nominal: 25000,
+  //       //   keterangan: "Beli Pulsa",
+  //       // },
+  //       // {
+  //       //   nominal: 15000,
+  //       //   keterangan: "Beli Bensin",
+  //       // },
+  //     ],
+  //   };
+  // },
+
+  // methods: {
+  //  entryPengeluaran(event){
+
+  //    this.dataPengeluaran.push(event);
+  //  }
+  // },
+  // computed: {
+  //   // showList: function() {
+  //   //   return this.dataPengeluaran.length > 0;
+  //   // },
+  //   // totalPengeluaran:function(){
+  //   //   return this.dataPengeluaran.reduce((acc, item) => acc + parseInt(item.nominal),0);
+  //   // }
+  // },
+  // watch:{
+  //   totalPengeluaran:function(val){
+  //     if(val > 100000){
+  //         this.warning= true
+  //     }
+  //   }
+  // }
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+  .t-pengeluaran{
+    text-align: center;
+  }
+</style>
--- src/components/FormPengeluaran.vue
+++ src/components/FormPengeluaran.vue
@@ -1,12 +1,12 @@
 <template>
-  <form class="form-pengeluaran" @submit.prevent="catat" >
+  <form class="form-pengeluaran" @submit.prevent="catat">
     <div>
       <label for="pengeluaran">Pengeluaran</label>
-      <input v-model="pengeluaran" type="number" id="pengeluaran">
+      <input v-model="state.pengeluaran" type="number" id="pengeluaran" />
     </div>
     <div>
-        <label for="keterangan">Keterangan</label>
-        <input v-model="keterangan" type="text" id="keterangan">
+      <label for="keterangan">Keterangan</label>
+      <input v-model="state.keterangan" type="text" id="keterangan" />
     </div>
     <div>
       <button>Catat!</button>
@@ -15,70 +15,92 @@
 </template>
 
 <script>
+// COMPOSITOION API
+import { reactive } from "vue";
 export default {
-  name:"FormPengeluaran",
-  data(){
-   return{
+  name: "FormPengeluaran",
+  setup(props, context) {
+    const state = reactive({
       pengeluaran: null,
-      keterangan: null
-   }
-  },
-  mounted(){
+      keterangan: null,
+    });
 
-  },
-  methods:{
-    catat(){
-      // emit child to parent
-      this.$emit('entry-pengeluaran', {
-        nominal: this.pengeluaran,
-        keterangan: this.keterangan
+    function catat() {
+      context.emit("entry-pengeluaran", {
+        nominal: state.pengeluaran,
+        keterangan: state.keterangan,
       });
-
-     
-      this.pengeluaran=null
-      this.keterangan=null
-
-      document.getElementById("pengeluaran").focus()
-
+      state.pengeluaran = null;
+      state.keterangan = null;
+      document.getElementById("pengeluaran").focus();
     }
-  }
-}
+
+    return {
+      state,
+      catat,
+    };
+  },
+  // OPTION API
+
+  // data(){
+  //  return{
+  //     pengeluaran: null,
+  //     keterangan: null
+  //  }
+  // },
+
+  // methods:{
+  //   catat(){
+  //     // emit child to parent
+  //     this.$emit('entry-pengeluaran', {
+  //       nominal: this.pengeluaran,
+  //       keterangan: this.keterangan
+  //     });
+
+  //     this.pengeluaran=null
+  //     this.keterangan=null
+
+  //     document.getElementById("pengeluaran").focus()
+
+  //   }
+  // }
+};
 </script>
 
 <style scoped>
-  .form-pengeluaran{
-      padding: 20px;
-      display: flex;
-      flex-flow: nowrap;
-      align-items: center;
-      justify-content: center;
-  }
+.form-pengeluaran {
+  padding: 20px;
+  display: flex;
+  flex-flow: nowrap;
+  align-items: center;
+  justify-content: center;
+}
 
-  .form-pengeluaran label{
-    margin: 5px 10px 5px 0;
-  }
+.form-pengeluaran label {
+  margin: 5px 10px 5px 0;
+}
 
-  .form-pengeluaran input{
-    vertical-align: middle;
-    margin: 5px 10px 5px 0;
-    padding: 10px;
-    background-color: #ffff;
-    border: 1px solid #ddd;
-  }
+.form-pengeluaran input {
+  vertical-align: middle;
+  margin: 5px 10px 5px 0;
+  padding: 10px;
+  background-color: #ffff;
+  border: 1px solid #ddd;
+}
 
-  .form-pengeluaran button:hover{
-    background-color: white;
-    color: blueviolet;
-    outline: none;
-    border: 1px solid blueviolet;
-  }
+.form-pengeluaran button:hover {
+  background-color: white;
+  color: blueviolet;
+  outline: none;
+  border: 1px solid blueviolet;
+}
 
-  .form-pengeluaran button{
-    padding: 10px 20px;
-    background-color: blueviolet;
-    border-radius: 10px;
-    color: white;
-    border: 1px solid blueviolet;
-    cursor: pointer;
-  }
+.form-pengeluaran button {
+  padding: 10px 20px;
+  background-color: blueviolet;
+  border-radius: 10px;
+  color: white;
+  border: 1px solid blueviolet;
+  cursor: pointer;
+}
 </style>
